16b153bdd8013b487384058c4ed6f5a94ecef108
zaiyong <zaiyongs@gmail.com>
2016-12-04 16:30:31 +0800

    adg

---
+++ app/src/main/java/com/abcew/model/composite/section2/ILeaf.java
@@ -0,0 +1,12 @@
+package com.abcew.model.composite.section2;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ * 叶子节点，也就是最小的小兵了，只能自己干活，不能指派别人了
+ */
+public interface ILeaf extends ICorp{
+	
+
+
+}
---
+++ app/src/main/java/com/abcew/model/observer/section1/Observer.java
@@ -0,0 +1,12 @@
+package com.abcew.model.observer.section1;
+
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ */
+public interface Observer {
+	
+	//更新方法
+	public void update();
+}
